f825747b166e57cb6cec101428ad6516d4b32851
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-28 08:57:30 -0300

    style: format rest

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,17 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
   brand: {
-    white: '#dcdcdc',
-    dark: '#9e1111',
-    light: '#c60f0f',
-    black: '#0f0f0f'
-  }
-}
+    white: "#dcdcdc",
+    dark: "#9e1111",
+    light: "#c60f0f",
+    black: "#0f0f0f",
+  },
+};
 
 const fonts = {
-  heading: 'Roboto',
-  body: 'Roboto'
-}
+  heading: "Roboto",
+  body: "Roboto",
+};
 
-export const theme = extendTheme({ colors, fonts })
+export const theme = extendTheme({ colors, fonts });
