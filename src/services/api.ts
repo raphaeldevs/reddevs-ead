9aefbf75aa4ca29e35b05d00451dd44e8f75ba94
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-28 08:33:30 -0300

    refactor: leaves axios base url dynamic

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+
+const isDev = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
+const baseURL = isDev
+  ? `http://${VERCEL_URL}/api`
+  : `https://${VERCEL_URL}/api`;
+
+console.log("__AXIOS BASE URL__", baseURL);
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
 });
