df8474c66defd96fe58be74123041424d5b3170e
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-31 00:41:31 -0300

    feat: add montserrat font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
